eaadd8a803c269f487487d7d449e4ec0522318cc
Drew Smith <smithdrew555@yahoo.com>
2022-09-21 15:59:31 -0400

    added named capture groups

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,14 @@ struct token {
 
 template <size_t N> struct fixed_string { char str[N]{}; };
 
-template <typename... tokens> constexpr auto gen_regex() {
-    fixed_string<(tokens::regex.size() + ...) + sizeof...(tokens) * 3> res{};
+template <typename... Tokens> constexpr auto gen_regex() {
+    fixed_string<(Tokens::regex.size() + ...) + sizeof...(Tokens) * 3> res{};
     auto str_itr = std::begin(res.str);
     (
         [&]() {
             *str_itr = '(';
             ++str_itr;
-            for (auto c : tokens::regex) {
+            for (auto c : Tokens::regex) {
                 *str_itr = c;
                 ++str_itr;
             }
@@ -32,15 +32,16 @@ template <typename... tokens> constexpr auto gen_regex() {
     return res;
 }
 
-template <int index, typename Variant, typename currToken, typename... tokens>
-Variant get_token(const auto &match) {
-    if constexpr (sizeof...(tokens) == 0) {
-        return currToken{match};
+template <typename Variant, typename CurrToken, typename... Tokens>
+constexpr Variant get_token(const auto &match) {
+    auto m = match.template get<CurrToken::capture_name>();
+    if constexpr (sizeof...(Tokens) == 0) {
+        return CurrToken{m};
     } else {
-        if (match.template get<index>()) {
-            return currToken{match};
+        if (m) {
+            return CurrToken{m};
         } else {
-            return get_token<index + 1, Variant, tokens...>(match);
+            return get_token<Variant, Tokens...>(match);
         }
     }
 }
@@ -50,7 +51,7 @@ template <typename Variant, typename IterType> struct tokenize_result {
     IterType end;
 };
 
-template <typename... tokens> auto tokenize(std::string_view v) {
+template <typename... tokens> constexpr auto tokenize(std::string_view v) {
     using Variant = std::variant<tokens...>;
     std::vector<Variant> token_vec;
     auto end = v.begin();
@@ -58,7 +59,7 @@ template <typename... tokens> auto tokenize(std::string_view v) {
     auto token_range = ctre::tokenize<gen_regex<tokens...>().str>(v);
     for (const auto &match : token_range) {
         if (match) {
-            token_vec.push_back(get_token<1, Variant, tokens...>(match));
+            token_vec.push_back(get_token<Variant, tokens...>(match));
         }
         end = match.end();
     }
@@ -66,22 +67,22 @@ template <typename... tokens> auto tokenize(std::string_view v) {
 }
 
 struct identifier : public token {
-    constexpr static std::string_view regex = "[a-z]+";
+    constexpr static ctll::fixed_string capture_name = "identifier";
+    constexpr static std::string_view regex =
+        "\\s*(?<identifier>[a-zA-Z][a-zA-Z0-9_]*)\\s*";
 };
 struct number : public token {
-    constexpr static std::string_view regex = "[0-9]+";
-};
-struct whitespace : public token {
-    constexpr static std::string_view regex = "\\s+";
+    constexpr static ctll::fixed_string capture_name = "number";
+    constexpr static std::string_view regex = "\\s*(?<number>[0-9]+)\\s*";
 };
 
 int main() {
-    std::string_view str = "test test";
-    auto [tokens, end] = tokenize<identifier, number, whitespace>(str);
-    if (str.end() - end > 0) {
+    std::string_view str = "test\t\n test 89 ";
+    auto [tokens, end] = tokenize<identifier, number>(str);
+    if (str.end() != end) {
         std::cout << "Invalid token at: " << end - str.begin() << '\n';
     }
     for (auto item : tokens) {
-        std::visit([](auto &&arg) { std::cout << arg.c << '\n'; }, item);
+        std::visit([](auto &&arg) { std::cout << arg.c << '.'; }, item);
     }
 }
