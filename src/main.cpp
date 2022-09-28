132f1cb76da1a34962e8132bbfdaddaf0a81c6fc
Drew Smith <smithdrew555@gmail.com>
2022-09-28 12:40:14 -0400

    simplified main

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,17 @@
 #include "script/script.h"
 #include <string>
 
-Variable print(Variable to_print) {
-    std::visit(
-        Overload{[](const auto &val) { std::cout << val << '\n'; },
-                 [](std::vector<Variable>) { std::cout << "vector\n"; },
-                 [](Variable (*)(Variable)) { std::cout << "function\n"; }},
-        to_print.data);
-    return Variable{0};
-}
-
 int main() {
     std::map<std::string, Variable> variables{};
 
-    variables["print"] = Variable{print};
+    variables["print"] = Variable{[](Variable to_print) {
+        std::visit(
+            Overload{[](const auto &val) { std::cout << val << '\n'; },
+                     [](std::vector<Variable>) { std::cout << "vector\n"; },
+                     [](Variable (*)(Variable)) { std::cout << "function\n"; }},
+            to_print.data);
+        return Variable{0};
+    }};
 
     std::string s;
     while (true) {
@@ -27,19 +25,12 @@ int main() {
             }
             auto p = parse_expression(s);
             std::visit(
-                Overload{[&](const AddExpression &a) {
-                             std::get<int>(a.evaluate(variables).data);
-                         },
+                Overload{[&](const AddExpression &a) { a.evaluate(variables); },
                          [&](const Assignment &a) {
-                             auto value = a.a.evaluate(variables);
-                             variables[std::string{a.i.str}] = value;
+                             variables[std::string{a.i.str}] =
+                                 a.a.evaluate(variables);
                          },
-                         [&](const IfExpression &i) {
-                             if (auto value = i.evaluate(variables)) {
-                                 std::cout << std::get<int>(value.value().data)
-                                           << '\n';
-                             }
-                         },
+                         [&](const IfExpression &i) { i.evaluate(variables); },
                          [](const ParseError &e) { std::cout << e << '\n'; },
                          [](const TokenizeError &e) { std::cout << e << '\n'; },
                          [](const auto &o) {
