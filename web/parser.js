c069ad09faaafc0f5abe4b2af2af537691024fee
Drew Smith <smithdrew555@yahoo.com>
2022-10-26 17:13:08 -0400

    extracted constanst in js

--- web/parser.js
+++ web/parser.js
@@ -1,6 +1,8 @@
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const output = document.getElementById("output");
 var Module = {
     print(text) {
-        let output = document.getElementById("output");
         let p = document.createElement("p")
         p.textContent = text;
         output.appendChild(p);
@@ -8,9 +10,6 @@ var Module = {
     printErr(text) { console.warn(text); },
     onRuntimeInitialized() {
         let wasm_parse = this.cwrap("wasm_parse", null, ["string"]);
-        let input = document.getElementById("input");
-        let button = document.getElementById("button");
-        let output = document.getElementById("output");
         button.onclick = () => {
             let p = document.createElement("code");
             p.textContent = input.value;
